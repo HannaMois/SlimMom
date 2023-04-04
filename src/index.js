0d16931daf9905696e2997288276821abda22a98
HannaMois <hannamois135@gmail.com>
2023-04-04 17:08:01 +0300

    Add changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/slim-mom/">
+    <BrowserRouter basename="/SlimMom/">
       <PersistGate loading={null} persistor={persistor}>
         <Provider store={store}>
           <App />
